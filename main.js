644e3b6c52a85b988ca7475c1aff9f5f2d6d246e
Ted Bell <tchbell@gmail.com>
2018-01-11 15:55:37 -0500

    When start button is clicked computer pick as many cells as there are rounds and a border is added and taken away to show user which was selected

---
+++ main.js
@@ -0,0 +1,41 @@
+//holds the computer logic for simon game
+let simonComputer = {
+  //contains what round the game is currently on
+  round: 0,
+  //contains which cell was chosen by the computer
+  simonMoves: [],
+  //randomly selected one of the cells every 750 ms
+  gridChoice: function(){
+    //gets called as many times are there are rounds
+    for(let i=0; i<this.round; i++){
+      //IFFE so that each iteration is called with the delay
+      (function(i){
+        setTimeout(function(){
+          let simonSaid = Math.floor(Math.random() * (5 - 1)) + 1;
+          let cell = document.getElementById(simonSaid);
+          //adds highlighted class so player can see what cell was selected
+          cell.classList.add('highlighted');
+          //removes highlighted class so there is no visual confusion
+          setTimeout(function(){
+            cell.classList.remove('highlighted');
+          }, 720);
+          console.log(simonSaid);
+          //puts the chosen cell into simonMoves
+          simonComputer.simonMoves.push(simonSaid);
+        }, 750 * i);
+      })(i);
+    }
+  }
+};
+
+//holds the button handlers
+let handlers ={
+  startBtn: function(){
+    let startButton =  document.querySelector('.start');
+    startButton.addEventListener('click', function(){
+      simonComputer.round ++;
+      simonComputer.gridChoice();
+    })
+  }
+}
+handlers.startBtn();
